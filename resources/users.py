419664042bcdceca538fd55fee8228c2129216a9
JamesRJGray <jamesrjgray@gmail.com>
2023-06-08 21:14:17 -0500

    added password update route for user passwords

--- resources/users.py
+++ resources/users.py
@@ -1,6 +1,6 @@
 import models
 from flask import Blueprint, jsonify, request
-from flask_bcrypt import check_password_hash
+from flask_bcrypt import check_password_hash, generate_password_hash
 from flask_login import login_user, logout_user, current_user, login_required
 from playhouse.shortcuts import model_to_dict
 
@@ -76,6 +76,37 @@ def logout():
 
     return jsonify({
         'message': 'Logout successful',
+        'status': 200
+    }), 200
+
+# User update password route
+@users.route('/password', methods=['PUT'])
+@login_required
+def update_password():
+    # fetch logged in user
+    user = models.User.get_by_id(current_user.id)
+
+    # Get the new password from request
+    data = request.get_json()
+    new_password = data.get('new_password')
+
+    # Check if the current password provided matches the user's actual password
+    current_password = data.get('current_password')
+    if not check_password_hash(user.password, current_password):
+        return jsonify({
+            'message': 'Current password is incorrect',
+            'status': 401
+        }), 401
+    
+    # generate hash for new pw
+    new_password_hash = generate_password_hash(new_password)
+
+    # Update the user's password
+    user.password = new_password_hash
+    user.save()
+
+    return jsonify({
+        'message': 'Password updated successfully',
         'status': 200
     }), 200
 
