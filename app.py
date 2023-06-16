088b07dc55cbdc15e39432610e271053b2dba59e
JamesRJGray <jamesrjgray@gmail.com>
2023-06-15 21:03:20 -0500

    changed CORS

--- app.py
+++ app.py
@@ -26,19 +26,22 @@ def load_user(user_id):
 
 
 # CORS arguments go here
-CORS(app, resources={r'/api/v1/*': {'origins': ['http://localhost:3000', 'https://quoticus.netlify.app']}}, supports_credentials=True)
+CORS(app, resources={r'/api/v1/*': {'origins': '*'}}, supports_credentials=True)
+CORS(app, resources={r'/*': {'origins': '*'}}, supports_credentials=True)
+
+# CORS(app, resources={r'/api/v1/*': {'origins': ['http://localhost:3000', 'https://quoticus.netlify.app']}}, supports_credentials=True)
 # CORS(app)
 # CORS(app, resources={r'/*': {'origins': ['http://localhost:3000', 'https://quoticus.netlify.app']}}, supports_credentials=True)
 # CORS(authors, origins='http://localhost:3000', supports_credentials=True)
 # CORS(categories, origins='http://localhost:3000', supports_credentials=True)
 # CORS(users, origins='http://localhost:3000', supports_credentials=True)
 # @app.after_request
-def set_cors_headers(response):
-    response.headers['Access-Control-Allow-Origin'] = 'https://quoticus.netlify.app' 
-    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS, HEAD'
-    response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
-    response.headers['Access-Control-Allow-Credentials'] = 'true'
-    return response
+# def set_cors_headers(response):
+#     response.headers['Access-Control-Allow-Origin'] = 'https://quoticus.netlify.app' 
+#     response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS, HEAD'
+#     response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+#     response.headers['Access-Control-Allow-Credentials'] = 'true'
+#     return response
 
 # Register blueprints with the app
 app.register_blueprint(authors, url_prefix='/api/v1/authors')
