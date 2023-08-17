ceaa75e0ea6d7722799275c0f7bfbf0985879d87
James Gray <jamesrjgray@gmail.com>
2023-08-16 23:00:19 -0500

    CORS logic for options request before redo of user auth

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 import models
 import os
@@ -16,6 +16,11 @@ login_manager = LoginManager()
 
 app = Flask(__name__)
 
+# Test Route for http://localhost:8000
+@app.route('/')
+def home():
+    return "Hello, this is the home page!"
+
 app.secret_key = 'VENIVEDIVICI'
 
 login_manager.init_app(app)
@@ -26,22 +31,30 @@ def load_user(user_id):
 
 
 # CORS arguments go here
-CORS(app, resources={r'/*': {'origins': '*'}}, supports_credentials=True)
-
-@app.after_request
-def add_cors_headers(response):
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
-    response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
-    response.headers['Access-Control-Allow-Credentials'] = 'true'
-    return response
+cors = CORS(app, resources={
+    r"/api/*": {
+        "origins": ["http://localhost:3000", "https://main--quoticus.netlify.app"],
+        "supports_credentials": True
+    }
+})
 
 # Register blueprints with the app
 app.register_blueprint(authors, url_prefix='/api/v1/authors')
 app.register_blueprint(categories, url_prefix='/api/v1/categories')
 app.register_blueprint(users, url_prefix='/api/v1/users')
 
-
+# Handling OPTIONS requests
+@app.before_request
+def handle_options_request():
+    if request.method == 'OPTIONS':
+        # Handle OPTIONS request
+        response = jsonify()
+        response.status_code = 200  # Set the response status to 200
+        response.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'  # Adjust origin as needed
+        response.headers['Access-Control-Allow-Methods'] = 'POST'
+        response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+        response.headers['Access-Control-Allow-Credentials'] = 'true'
+        return response
 
 if __name__ == '__main__':
     models.initialize()
